a75c18b5d99704140a718f39cbafcab44fc4bf3f
saurav chauhan <145248513+sauravchauhan2003@users.noreply.github.com>
2025-01-30 18:52:17 +0530

    Create Generateprimes.cpp

---
+++ Generateprimes.cpp
@@ -0,0 +1,23 @@
+vector<int> generatePrimes(int n) {
+    vector<bool> isPrime(n + 1, true);
+    vector<int> primes;
+
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            primes.push_back(i);
+        }
+    }
+
+    return primes;
+}
